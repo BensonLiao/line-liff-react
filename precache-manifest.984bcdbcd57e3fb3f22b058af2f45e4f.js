68515a56a9fb252c62d517e413fec5678b803b26
BensonLiao <skylock777@gmail.com>
2019-07-09 15:41:29 +0800

    Updates

---
+++ precache-manifest.984bcdbcd57e3fb3f22b058af2f45e4f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88bb7fd1f83895781cc24c5fe05afd80",
+    "url": "/liff-react-boilerplate/index.html"
+  },
+  {
+    "revision": "9a80f36aa36946e40cf6",
+    "url": "/liff-react-boilerplate/static/css/2.abcc5b61.chunk.css"
+  },
+  {
+    "revision": "90d46ec23b691bdbd539",
+    "url": "/liff-react-boilerplate/static/css/main.d7d431e3.chunk.css"
+  },
+  {
+    "revision": "9a80f36aa36946e40cf6",
+    "url": "/liff-react-boilerplate/static/js/2.3b90296d.chunk.js"
+  },
+  {
+    "revision": "90d46ec23b691bdbd539",
+    "url": "/liff-react-boilerplate/static/js/main.04c32d52.chunk.js"
+  },
+  {
+    "revision": "efde0d75e54efd131e1e",
+    "url": "/liff-react-boilerplate/static/js/runtime~main.8a8a984e.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/liff-react-boilerplate/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/liff-react-boilerplate/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/liff-react-boilerplate/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/liff-react-boilerplate/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/liff-react-boilerplate/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6b982374de8d33d5843e41a4d1ad3267.js"
+  "/liff-react-boilerplate/precache-manifest.984bcdbcd57e3fb3f22b058af2f45e4f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/liff-react-boilerplate/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
