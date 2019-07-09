0e69b3908c934136bec21c813a16d2567f08be88
BensonLiao <skylock777@gmail.com>
2019-07-09 14:11:59 +0800

    Change SendMessage's input to datalist when messsage type is text

--- src/pages/SendMessage.js
+++ src/pages/SendMessage.js
@@ -1,188 +1,242 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import swal from 'sweetalert2'
-import { geolocated } from 'react-geolocated'
-import liffHelper from '../utils/liffHelper'
-import messageHelper from '../utils/messagingApiHelper'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import swal from "sweetalert2";
+import { geolocated } from "react-geolocated";
+import liffHelper from "../utils/liffHelper";
+import messageHelper from "../utils/messagingApiHelper";
+
+const textOptions = [
+  "give me brown",
+  "search imgur account",
+  "upload to imgur"
+];
+
+const datalistId = "text-options";
 
 const messageTypes = [
-  { 
-    key: 'text', 
-    label: 'Text', 
-    editable: true, 
-    value: 'give me brown' 
+  {
+    key: "text",
+    label: "Text",
+    editable: true,
+    value: ""
   },
-  { 
-    key: 'image', 
-    label: 'Image', 
-    editable: true, 
-    value: 'https://developers.line.biz/media/messaging-api/using-line-url-scheme/camera-screen-3c7466e3.png' 
+  {
+    key: "image",
+    label: "Image",
+    editable: true,
+    value:
+      "https://developers.line.biz/media/messaging-api/using-line-url-scheme/camera-screen-3c7466e3.png"
   },
-  { 
-    key: 'video', 
-    label: 'Video', 
-    editable: false, 
-    value: 'https://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_5mb.mp4' 
+  {
+    key: "video",
+    label: "Video",
+    editable: false,
+    value:
+      "https://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_5mb.mp4"
   },
-  { 
-    key: 'audio', 
-    label: 'Audio', 
-    editable: false, 
-    value: 'https://cdn.online-convert.com/example-file/audio/m4a/example.m4a' 
+  {
+    key: "audio",
+    label: "Audio",
+    editable: false,
+    value: "https://cdn.online-convert.com/example-file/audio/m4a/example.m4a"
   },
-  { 
-    key: 'location', 
-    label: 'Location', 
-    editable: false, 
-    value: 'Location' 
+  {
+    key: "location",
+    label: "Location",
+    editable: false,
+    value: "Location"
   },
-  { 
-    key: 'button', 
-    label: 'Template - Button', 
-    editable: false, 
-    value: 'Button' 
+  {
+    key: "button",
+    label: "Template - Button",
+    editable: false,
+    value: "Button"
   },
-  { 
-    key: 'confirm', 
-    label: 'Template - Confirm', 
-    editable: false, 
-    value: 'Confirm' 
+  {
+    key: "confirm",
+    label: "Template - Confirm",
+    editable: false,
+    value: "Confirm"
   },
-  { 
-    key: 'carousel', 
-    label: 'Template - Carousel', 
-    editable: false, 
-    value: 'Carousel', 
-    disabled: true 
+  {
+    key: "carousel",
+    label: "Template - Carousel",
+    editable: false,
+    value: "Carousel",
+    disabled: true
   },
-  { 
-    key: 'image-carousel', 
-    label: 'Template - Image carousel', 
-    editable: false, 
-    value: 'Image carousel', 
-    disabled: true 
-  },
-]
+  {
+    key: "image-carousel",
+    label: "Template - Image carousel",
+    editable: false,
+    value: "Image carousel",
+    disabled: true
+  }
+];
 
 class SendMessage extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    this.textInput = []
+    this.textInput = [];
 
     this.setTextInputRef = (key, element) => {
-      this.textInput[key] = element
-    }
+      this.textInput[key] = element;
+    };
   }
 
   sendMessageToChat(messageKey) {
-    let value = this.textInput[messageKey].value
-    let message
+    let value = this.textInput[messageKey].value;
+    let message;
     switch (messageKey) {
-      case 'text':
-        message = messageHelper.createTextMessage(value); break
-      case 'image':
-        message = messageHelper.createImageMessage(value, value); break
-      case 'video':
-        let video = 'https://www.sample-videos.com/img/Sample-png-image-500kb.png'
-        message = messageHelper.createVDOMessage(value, video); break
-      case 'audio':
-        message = messageHelper.createAudioMessage(value, 3600); break
-      case 'button':
+      case "text":
+        message = messageHelper.createTextMessage(value);
+        break;
+      case "image":
+        message = messageHelper.createImageMessage(value, value);
+        break;
+      case "video":
+        let video =
+          "https://www.sample-videos.com/img/Sample-png-image-500kb.png";
+        message = messageHelper.createVDOMessage(value, video);
+        break;
+      case "audio":
+        message = messageHelper.createAudioMessage(value, 3600);
+        break;
+      case "button":
         let actions = [
           {
-            "type": "uri",
-            "label": "Google",
-            "uri": `https://google.com`
+            type: "uri",
+            label: "Google",
+            uri: `https://google.com`
           },
           {
-            "type": "uri",
-            "label": "Facebook",
-            "uri": `https://facebook.com`
-          },
-        ]
-        message = messageHelper.createButtonMessageWithImage('Select Page', 'Send Buttton Demo', 'https://developers.line.biz/media/messaging-api/using-line-url-scheme/camera-screen-3c7466e3.png', actions)
-        break
-      case 'confirm':
+            type: "uri",
+            label: "Facebook",
+            uri: `https://facebook.com`
+          }
+        ];
+        message = messageHelper.createButtonMessageWithImage(
+          "Select Page",
+          "Send Buttton Demo",
+          "https://developers.line.biz/media/messaging-api/using-line-url-scheme/camera-screen-3c7466e3.png",
+          actions
+        );
+        break;
+      case "confirm":
         let confirmActions = [
           {
-            "type": "uri",
-            "label": "YES",
-            "uri": `https://google.com`
+            type: "uri",
+            label: "YES",
+            uri: `https://google.com`
           },
           {
-            "type": "uri",
-            "label": "NO",
-            "uri": `https://facebook.com`
+            type: "uri",
+            label: "NO",
+            uri: `https://facebook.com`
           }
-        ]
-        message = messageHelper.createConfirmMessage('Send Message again', confirmActions)
-        break
-      case 'location':
-        if (this.props.isGeolocationAvailable && this.props.isGeolocationEnabled) {
-          let lat = this.props.coords.latitude
-          let lng = this.props.coords.longitude
-          message = messageHelper.createLocationMessage(lat, lng)
+        ];
+        message = messageHelper.createConfirmMessage(
+          "Send Message again",
+          confirmActions
+        );
+        break;
+      case "location":
+        if (
+          this.props.isGeolocationAvailable &&
+          this.props.isGeolocationEnabled
+        ) {
+          let lat = this.props.coords.latitude;
+          let lng = this.props.coords.longitude;
+          message = messageHelper.createLocationMessage(lat, lng);
         } else {
           swal({
-            type: 'error',
-            title: 'Send Error',
-            text: 'Geolocation is not enabled',
-          })
+            type: "error",
+            title: "Send Error",
+            text: "Geolocation is not enabled"
+          });
         }
-        break
+        break;
       default:
-        message = messageHelper.createTextMessage('ABC')
+        message = messageHelper.createTextMessage("ABC");
     }
     if (message) {
-      liffHelper.sendMessages([message])
+      liffHelper
+        .sendMessages([message])
         .then(() => {
           swal({
-            type: 'success',
-            title: 'Send Message Complete',
+            type: "success",
+            title: "Send Message Complete",
             showConfirmButton: false,
             timer: 1000
-          })
+          });
         })
-        .catch((err) => {
+        .catch(err => {
           swal({
-            type: 'error',
-            title: 'Send Error',
-            text: err.response.data.message,
-          })
-        })
+            type: "error",
+            title: "Send Error",
+            text: err.response.data.message
+          });
+        });
     }
   }
 
+  getInputElement(messageType) {
+    return messageType.key === "text" ? (
+      <input
+        ref={this.setTextInputRef.bind(this, messageType.key)}
+        id={`msg_${messageType.key}`}
+        disabled={!messageType.editable}
+        type="text"
+        list={datalistId}
+        className="form-control"
+        placeholder="Type some message or choose from list"
+      />
+    ) : (
+      <input
+        ref={this.setTextInputRef.bind(this, messageType.key)}
+        id={`msg_${messageType.key}`}
+        disabled={!messageType.editable}
+        type="text"
+        className="form-control"
+        defaultValue={messageType.value}
+      />
+    );
+  }
+
   renderMessageTypeKey() {
-    return messageTypes.map((messageType) => (
+    return messageTypes.map(messageType => (
       <div className="form-group" key={messageType.key}>
-        <label 
-          htmlFor={`msg_${messageType.key}`} 
-          className="message-label">
+        <label htmlFor={`msg_${messageType.key}`} className="message-label">
           {messageType.label}:
         </label>
         <div className="input-group">
-          <input 
-            ref={this.setTextInputRef.bind(this, messageType.key)} 
-            id={`msg_${messageType.key}`} 
-            disabled={!messageType.editable} 
-            type="text" 
-            className="form-control" 
-            defaultValue={messageType.value} 
-          />
+          {this.getInputElement(messageType)}
+          {/* note. datalist are not supported on some browser */}
+          {/* ref: https://caniuse.com/#search=datalist */}
+          {/* if in production, it should be handle properly with detect or fallback */}
+          {messageType.key === "text" && (
+            <datalist id={datalistId}>
+              {textOptions.map(option => (
+                <option value={option} key={option}>
+                  {option}
+                </option>
+              ))}
+            </datalist>
+          )}
           <span className="input-group-btn">
-          <button 
-            type="button" 
-            className="btn btn-default" 
-            disabled={messageType.disabled} 
-            onClick={this.sendMessageToChat.bind(this, `${messageType.key}`)} >
-            Send
-          </button>
+            <button
+              type="button"
+              className="btn btn-default"
+              disabled={messageType.disabled}
+              onClick={this.sendMessageToChat.bind(this, `${messageType.key}`)}
+            >
+              Send
+            </button>
           </span>
         </div>
       </div>
-    ))
+    ));
   }
 
   render() {
@@ -192,25 +246,28 @@ class SendMessage extends Component {
         <div className="col-lg-6">
           {this.renderMessageTypeKey()}
           <hr />
-          <button 
-            type="button" 
-            className="btn btn-default" 
-            onClick={() => { liffHelper.closeWindow() }}>
+          <button
+            type="button"
+            className="btn btn-default"
+            onClick={() => {
+              liffHelper.closeWindow();
+            }}
+          >
             Close LIFF
           </button>
         </div>
         <div className="col-lg-3" />
       </div>
-    )
+    );
   }
 }
 
 export default geolocated({
   positionOptions: {
-    enableHighAccuracy: true,
+    enableHighAccuracy: true
   },
-  userDecisionTimeout: 5000,
-})(SendMessage)
+  userDecisionTimeout: 5000
+})(SendMessage);
 
 //For later implementation
 SendMessage.propTypes = {
@@ -218,6 +275,6 @@ SendMessage.propTypes = {
   isGeolocationEnabled: PropTypes.bool,
   coords: PropTypes.shape({
     latitude: PropTypes.number,
-    longitude: PropTypes.number,
-  }),
-}
+    longitude: PropTypes.number
+  })
+};
